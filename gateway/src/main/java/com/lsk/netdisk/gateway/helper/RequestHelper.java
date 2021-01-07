b79326cbb853b1035e8e645d95730066f4e8db21
lsk-china <55624809+lsk-china@users.noreply.github.com>
2021-01-07 18:45:07 +0800

    commit

--- gateway/src/main/java/com/lsk/netdisk/gateway/helper/RequestHelper.java
+++ gateway/src/main/java/com/lsk/netdisk/gateway/helper/RequestHelper.java
@@ -1,16 +1,26 @@
 package com.lsk.netdisk.gateway.helper;
 
+import com.lsk.netdisk.gateway.mapper.CookieMapper;
 import com.lsk.netdisk.gateway.mapper.ParamMapper;
 import com.lsk.netdisk.gateway.mapper.URLsMapper;
+import com.lsk.netdisk.gateway.models.Cookie;
 import com.lsk.netdisk.gateway.models.Param;
 import com.lsk.netdisk.gateway.models.URL;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,8 +34,12 @@ public class RequestHelper {
 	private URLsMapper urlMapper;
 	@Autowired
 	private ParamMapper paramMapper;
+	@Autowired
+	private CookieMapper cookieMapper;
+	@Autowired
+	private LoadBalancerClient loadBalancerClient;
 
-	public Object handleAuth(HttpServletRequest req, HttpServletResponse resp){
+	public Object handleAuth(HttpServletRequest req){
 		String path = req.getPathInfo();
 		URL url = urlMapper.queryURLByPath(path);
 		Integer urlID = url.getId();
@@ -36,6 +50,46 @@ public class RequestHelper {
 			String value = req.getParameter(name);
 			paramMap.put(name,value);
 		}
-		return null;
+		Map<String,String> cookieMap = new HashMap<>();
+		List<Cookie> cookies = cookieMapper.queryCookiesByURLID(urlID);
+		for(Cookie cookie : cookies){
+			String name = cookie.getName();
+			String value = getCookieValue(req,name);
+			cookieMap.put(name,value);
+		}
+		ServiceInstance serviceInstance = loadBalancerClient.choose("auth");
+		String address = "http://"+serviceInstance.getHost()+":"+serviceInstance.getPort()+path;
+		log.info(address);
+		HttpMethod method = HttpMethod.valueOf(req.getMethod());
+		return sendRequest(method,address,paramMap,cookieMap);
+	}
+	private Object sendRequest(HttpMethod method,String url, Map<String,String> params, Map<String,String> cookies){
+		List<String> cookieList = new ArrayList<>();
+		for(Map.Entry<String,String> entry : cookies.entrySet()){
+			String name = entry.getKey();
+			String value = entry.getValue();
+			cookieList.add(name+"="+value);
+		}
+		HttpHeaders httpHeaders = new HttpHeaders();
+		if(cookieList.size() > 0){
+			httpHeaders.put("Cookie",cookieList);
+		}
+		MultiValueMap<String,String> requestParams = new LinkedMultiValueMap<>();
+		for(Map.Entry<String,String> entry:params.entrySet()){
+			String name = entry.getKey();
+			String value = entry.getValue();
+			requestParams.add(name,value);
+		}
+		HttpEntity<MultiValueMap<String,String>> httpEntity = new HttpEntity<>(requestParams,httpHeaders);
+		return restTemplate.exchange(url,method,httpEntity,Object.class);
+	}
+	private String getCookieValue(HttpServletRequest req, String name){
+		javax.servlet.http.Cookie[] cookies = req.getCookies();
+		for (javax.servlet.http.Cookie cookie : cookies){
+			if (cookie.getName().equals(name)){
+				return cookie.getValue();
+			}
+		}
+		throw new RuntimeException("Cookie not found!");
 	}
 }
--- gateway/src/main/java/com/lsk/netdisk/gateway/models/Cookie.java
+++ gateway/src/main/java/com/lsk/netdisk/gateway/models/Cookie.java
@@ -1,4 +1,16 @@
 package com.lsk.netdisk.gateway.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Cookie {
+	private Integer id;
+	private String name;
+	private Integer urlID;
 }
